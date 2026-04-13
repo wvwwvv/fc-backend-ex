cb56bb46964f6cb6abb8cde8f74361f50265a370
inwoohub <dlsdndls@naver.com>
2026-04-13 19:34:59 +0900

    #60 feat: Thumbnailator 의존성 추가

--- src/test/java/com/fc/fcseoularchive/inwooTest/BetTest.java
+++ src/test/java/com/fc/fcseoularchive/inwooTest/BetTest.java
@@ -1,26 +1,18 @@
 package com.fc.fcseoularchive.inwooTest;
 
-import io.jsonwebtoken.Jwts;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.HashSet;
 
 @SpringBootTest
-public class jwtTest2 {
+public class BetTest {
 
     @Test
-    @DisplayName("토큰 구조 바뀌어서 id 위치 보기")
-    public void test1() throws Exception{
-
+    @DisplayName("현재 베팅 게임 정보 반환")
+    public void test1() throws Exception {
         LocalDateTime now = LocalDateTime.now();
-        System.out.println("now = " + now);
-
+        System.out.println(now);
     }
-
-
-
 }
